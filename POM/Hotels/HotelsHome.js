a292bf4d54bce099d1d0ea211d636c2bb904a7cd
Mashrur Elahi <elahi@macbook-pro-2.myfiosgateway.com>
2023-01-06 03:18:00 -0500

    firstSprintComplete

--- POM/Hotels/HotelsHome.js
+++ POM/Hotels/HotelsHome.js
@@ -12,22 +12,30 @@ class HotelsHome {
     languageDropDown = '#language-selector';
     saveButton = '//select[@id="language-selector"]/following::button';
     currentLanguage = '//button[@data-stid="button-type-picker-trigger"]//div/div';
-    customizeTravelers = '//button[@data-stid="open-room-picker"]';
+    travelersButton = '//button[@data-stid="open-room-picker"]';
     minusAdult = '//input[@aria-label="Adults "]/preceding-sibling::button';
     numOfAdults = '//input[@aria-label="Adults "]';
-    plusAdults = '//input[@aria-label="Adults "]/following-sibling::button';
+    plusAdult = '//input[@aria-label="Adults "]/following-sibling::button';
     adultMinusDisabled = '//input[@aria-label="Adults "]/preceding-sibling::button[@disabled]';
     adultPlusDisabled = '//input[@aria-label="Adults "]/following-sibling::button[@disabled]';
     datesCalendar = '//button[@data-stid="open-date-picker"]';
-    monthsDisplayed = '//h2[@class="uitk-date-picker-month-name uitk-type-medium"]';
+    currentMonthFinder_1 = '//h2[starts-with(text(),';
+    currentMonthFinder_2 = ')]';
     disabledDates = '//td//button[@disabled]';
     getAppButton = '#submitBtn';
     phoneNumber = '#phoneNumber';
     phoneNumberError = '#phoneNumber-error';
     signInButton = '//button[text()="Sign in"]';
     feedbackLink = '//a[text()="Feedback"]';
-    
+    prevMonths = '//button[@data-stid="date-picker-paging"]/preceding-sibling::button';
+    nextMonths = '//button[@data-stid="date-picker-paging"]/following-sibling::button';
+    numOfChildren = '//input[starts-with(@aria-label, "Children")]';
+    minusChildren = '//input[starts-with(@aria-label, "Children")]/preceding-sibling::button';
+    plusChildren = '//input[starts-with(@aria-label, "Children")]/following-sibling::button';
+    childAgeTo = '#age-traveler_selector_children_age_selector-0-';
+    doneButton = '//button[text()="Done"]';
 
+    
 
     // functions
 
@@ -96,21 +104,16 @@ class HotelsHome {
 
     async openDates () {
         await this.commands.clickElement(this.datesCalendar);
+        await this.commands.sleep(2);
     }
 
     async verifyMonth () {
 
-        const expectedDate = Dates.getCurrentDate('Month');
-        const monthElements = await this.commands.findAllWebElements(this.monthsDisplayed);
-        let displayedMonths = [];
-        for (let monthElement of monthElements) {
-            const thisMonth = await this.commands.getElementText(monthElement);
-            displayedMonths.push(thisMonth);
-        }
+        const expectedMonth = Dates.getCurrentDate('Month');
+        const locatorString = this.currentMonthFinder_1+`"${expectedMonth}"`+this.currentMonthFinder_2;
+        const isMonthPresent = await this.commands.isElementedDisplayed(locatorString);
 
-        console.log('\n-----');
-        console.log(displayedMonths);
-        console.log('-----\n');
+        return isMonthPresent;
 
     }
 
@@ -120,6 +123,167 @@ class HotelsHome {
 
     }
 
+    async inputPhoneNumber (phoneNumber) {
+
+        await this.commands.typeInField(this.phoneNumber, phoneNumber);
+
+    }
+
+    async getApp () {
+
+        await this.commands.clickElement(this.getAppButton);
+
+    }
+
+    async verifyPhoneNumber () {
+
+        return await this.commands.getElementText(this.phoneNumberError);
+
+    } 
+
+    async signIn () {
+
+        await this.commands.clickElement(this.signInButton);
+
+    }
+
+    async feedBack () {
+
+        await this.commands.clickElement(this.feedbackLink);
+
+    }
+
+    async viewEarlierMonths () {
+
+        await this.commands.clickElement(this.prevMonths);
+
+    }
+
+    async viewFutureMonths () {
+
+        await this.commands.clickElement(this.nextMonths);
+
+    }
+
+    async checkPastDates () {
+
+        const todaysDate = Dates.getCurrentDate('day');
+        const dateNumber = Number(todaysDate);
+        let booleanHolder = true
+
+        for (let i = dateNumber - 1 ; i > 0 ; i--) {
+            const thisDate = i.toString();
+            const isDateEnabled = await this.commands.isElementEnabled(`//button[starts-with(@aria-label, "Jan ${thisDate}")]`);
+            if (isDateEnabled === true) {
+                booleanHolder = false
+            }
+        }
+
+        return booleanHolder;
+
+    }
+
+    async prevMonthsDisabled () {
+
+        return await this.commands.isElementEnabled(this.prevMonths);
+
+    }
+
+    async customizeTravelers () {
+
+        await this.commands.clickElement(this.travelersButton);
+
+    }
+
+    async setAdultsTo (num) {
+
+        const currentAdults = Number(await this.commands.getAttrValue(this.numOfAdults, "value"));
+            if (num > currentAdults) {
+                for (let i = 1 ; i <= num - currentAdults ; i++) {
+                   await this.commands.clickElement(this.plusAdult);
+                }
+            } else if (num < currentAdults) {
+                for (let i = 1 ; i <= currentAdults - num ; i++) {
+                    await this.commands.clickElement(this.minusAdult);
+                }
+            }
+
+    }
+
+    async setChildrenTo (num) {
+
+        const currentChildren = Number(await this.commands.getAttrValue(this.numOfChildren, "value"));
+            if (num > currentChildren) {
+                for (let i = 1 ; i <= num - currentChildren ; i++) {
+                    await this.commands.clickElement(this.plusChildren);
+                }
+            } else if (num < currentChildren) {
+                for (let i = 1 ; i <= currentChildren - num ; i++) {
+                    await this.commands.clickElement(this.minusChildren);
+                }
+            }
+
+    }
+
+    async setChildAgeTo (childNum, childAge) {
+
+        const startValue = Number(childNum) - 1;
+        const locatorEnding = startValue.toString(); 
+        await this.commands.dropDownSelect(this.childAgeTo.padEnd(this.childAgeTo.length + 1, locatorEnding), childAge);
+
+    }
+
+    async submitTravelerInfo () {
+
+        await this.commands.clickElement(this.doneButton);
+
+    }
+
+    async verifyTravelerSum () {
+
+        const displayedTravelers = await this.commands.getElementText(this.travelersButton);
+        await this.commands.clickElement(this.travelersButton);
+        const totalAdults = Number(await this.commands.getAttrValue(this.numOfAdults, "value"));
+        const totalChildren = Number(await this.commands.getAttrValue(this.numOfChildren, "value"));
+        const totalTravelers = totalAdults + totalChildren;
+        const verifyTravelers = displayedTravelers.includes(totalTravelers.toString()+' travelers');
+
+        return verifyTravelers;
+
+    }
+
+    async setTravelerTo (traveler, num) {
+
+        if (traveler.toLowerCase === "adults") {
+            const currentAdults = Number(await this.commands.getAttrValue(this.numOfAdults, "value"));
+            if (num > currentAdults) {
+                for (let i = 1 ; i <= num - currentAdults ; i++) {
+                   await this.commands.clickElement(this.plusAdult);
+                }
+            } else if (num < currentAdults) {
+                for (let i = 1 ; i <= currentAdults - num ; i++) {
+                    await this.commands.clickElement(this.minusAdult);
+                }
+            }
+        } else if (traveler.toLowerCase() === "children") {
+
+            const currentChildren = Number(await this.commands.getAttrValue(this.numOfChildren, "value"));
+            if (num > currentChildren) {
+                for (let i = 1 ; i <= num - currentChildren ; i++) {
+                    await this.commands.clickElement(this.plusChildren);
+                }
+            } else if (num < currentChildren) {
+                for (let i = 1 ; i <= currentChildren - num ; i++) {
+                    await this.commands.clickElement(this.minusChildren);
+                }
+            }
+        }
+
+        await this.commands.sleep(5);
+        
+    }
+
 }
+
 
 module.exports = HotelsHome;
--- step-definitions/Hotels/hotelHomeSD.js
+++ step-definitions/Hotels/hotelHomeSD.js
@@ -10,14 +10,15 @@ const dWord = new DirectWord();
 
 
 Given(/^I am on hotels$/, async () => {
+
     await browser.url('https://www.hotels.com/');
+
 })
 
 When(/^I change language to (.+)$/, async (languageOption) => {
 
     await hotels.changeLanguage(languageOption);
 
-    
 })
 
 Then(/^I verify language got changed to (.+)$/, async (language) => {
@@ -30,72 +31,41 @@ Then(/^I verify language got changed to (.+)$/, async (language) => {
 
 })
 
-When(/^I select number of adults in Room 1 as 1$/, async() => {
 
-    await hotels.setAdultMinimum();
-})
 
-// Then(/^I verify the minus button for adults is (disabled|enabled)$/, async (isEnabled) => {
+When(/^I click on Travelers$/, async () => {
 
-//     const isMinusEnabled = await hotels.isMinusEnabled();
-//     switch (isEnabled) {
-//         case 'disabled':
-//             expect(isMinusEnabled, 'Minus button is still enabled').to.be.false;
-//             break;
-
-//         case 'enabled':
-//             expect(isMinusEnabled, 'Minus button is not enabled').to.be.true;
-//             break;
-    
-//         default:
-//             break;
-//     }
-
-// })
-
-Then(/^I verify the (plus|minus) button for adults is (disabled|enabled)$/, async (button, status) => {
-
-    switch (button) {
-        case 'plus':
-            const isPlusEnabled = await hotels.isPlusEnabled();
-            switch (status) {
-                case 'disabled':
-                    expect(isPlusEnabled, 'Plus button is still enabled').to.equal('true');
-                    break;
-
-                case 'enabled':
-                    expect(isPlusEnabled, 'Plus button is not enabled').to.equal('false');
-                    break;
-
-                default:
-                    break;
-            }
-            break;
-
-        case 'minus':
-            const isMinusEnabled = await hotels.isMinusEnabled();
-            switch (status) {
-                case 'disabled':
-                    expect(isMinusEnabled, 'Minus button is still enabled').to.equal('true');
-                    break;
-
-                case 'enabled':
-                    expect(isMinusEnabled, 'Minus button is not enabled').to.equal('false');
-                    break;
-            
-                default:
-                    break;
-            }
-
-        default:
-            break;
-    }
+    await hotels.customizeTravelers();
 
 })
 
-When(/^I select number of adults in Room 1 as 14$/, async () => {
+When(/^I set "Adults" to (.+)$/, async (num) => {
 
-    await hotels.setAdultMaximum();
+    await hotels.setAdultsTo(num);
+
+})
+
+When(/^I set "Children" to (.+)$/, async (num) => {
+
+    await hotels.setChildrenTo(num);
+
+})
+
+When(/^I set Child (.+) age to (.+)$/, async (childNum, childAge) => {
+
+    await hotels.setChildAgeTo(childNum, childAge);
+
+})
+
+When(/^I click Done$/, async () => {
+
+    await hotels.submitTravelerInfo();
+
+})
+
+Then(/^I verify total number of guests in sum of adults and children as same as selected on step #3 and #4$/, async () => {
+
+    expect(await hotels.verifyTravelerSum(), 'Incorrect total number of guests').to.be.true;
 
 })
 
@@ -107,14 +77,30 @@ When(/^I click on dates$/, async () => {
 
 When(/^I go to current month if not displayed$/, async () => {
 
-    await hotels.verifyMonth();
-    await browser.pause(2000);
+    const isCurrentMonthDisplayed = await hotels.verifyMonth();
+    if (!isCurrentMonthDisplayed) {
+        await hotels.viewEarlierMonths()
+    }
 
 })
 
-Then(/^I verify for correct month$/, async () => {
+Then(/^I verify for current month$/, async () => {
     
-    await hotels.verifyMonth();
+    expect(await hotels.verifyMonth(), 'Current month is not displayed').to.be.true;
+
+})
+
+Then(/^I verify past dates are disabled$/, async () => {
+
+    const dateStatus = await hotels.checkPastDates();
+    expect(dateStatus, 'Past dates are not all disabled').to.be.true;
+
+})
+
+Then(/^I verify back button on current month is disabled$/, async () => {
+    
+    const prevMonthButtonStatus = await hotels.prevMonthsDisabled();
+    expect(prevMonthButtonStatus, 'Previous month button is enabled').to.be.false;
 
 })
 
@@ -126,19 +112,19 @@ When(/^I scroll to "Get the app" button$/, async () => {
 
 When(/^I enter "(.+)" in Phone number$/, async (phoneNumber) => {
 
-    await commands.typeInField(hotels.phoneNumber, phoneNumber)
+    await hotels.inputPhoneNumber(phoneNumber);
 
 })
 
 When (/^I click on “Get the app“ button$/, async () => {
 
-    await commands.clickElement(hotels.getAppButton);
+    await hotels.getApp();
 
 })
 
 Then(/^I verify “Please enter a valid phone number.“ error is displayed$/, async () => {
 
-    const phoneError = await commands.getElementText(hotels.phoneNumberError);
+    const phoneError = await hotels.verifyPhoneNumber();
     const expectedError = 'Please enter a valid phone number.';
 
     expect(phoneError, 'Error message is not diplayed').to.equal(expectedError);
@@ -147,14 +133,14 @@ Then(/^I verify “Please enter a valid phone number.“ error is displayed$/, a
 
 When(/^I click on Sign In$/, async () => {
 
-    await commands.clickElement(hotels.signInButton);
+    await hotels.signIn();
 
 })
 
 When(/^I click feedback$/, async () => {
 
     const hotelsWindow = await commands.getHandle();
-    await commands.clickElement(hotels.feedbackLink);
+    await hotels.feedBack();
     const allWindows = await commands.getHandles();
     for (let window of allWindows) {
         if (window !== hotelsWindow) {
@@ -166,14 +152,14 @@ When(/^I click feedback$/, async () => {
 
 When(/^I click on Submit button$/, async () => {
 
-    await commands.clickElement(dWord.submitButton);
+    await dWord.submitInfo();
 
 })
 
 Then(/^I verify error message is displayed$/, async () => {
 
     const expectedErrorMessage = 'Please fill in the required information highlighted below.';
-    const errorMessageStatus = await commands.getElementText(dWord.errorMessage);
+    const errorMessageStatus = await dWord.getErrorMsg();
 
     expect(errorMessageStatus, 'Error message is not displayed').to.equal(expectedErrorMessage);
 
@@ -181,6 +167,105 @@ Then(/^I verify error message is displayed$/, async () => {
 
 Then(/^I verify star boxes section is in a red dotted box$/, async () => {
 
-    await browser.takeScreenshot();
+    expect(await dWord.isRedDottedBox(), 'Red dotted box is not displayed').to.be.true;
 
 })
+
+When(/^I rate the page (.+)$/, async (starRating) => {
+
+    await dWord.ratePage(starRating);
+
+})
+
+When(/^I enter "(.+)" in comments$/, async (text) => {
+
+    await dWord.enterComment(text);
+
+})
+
+When(/^I select the option "(.+)" for how likely are you to return to Hotels$/, async (selection) => {
+
+    await dWord.returnToHotelsDropDown(selection);
+
+})
+
+When(/^I select "(.+)" for have you ever booked on Hotels$/, async (selection) => {
+
+    await dWord.bookedBefore(selection);
+
+})
+
+When(/^I select "(.+)" for did you accomplish what you wanted to on this page$/, async (selection) => {
+
+    await dWord.pageAccomplish(selection);
+
+})
+
+Then(/^I verify "THANK YOU FOR YOUR FEEDBACK" message is displayed$/, async () => {
+
+    await commands.sleep(3);
+
+    const thankYouMssg = await dWord.verifyThankYouMessage();
+    expect(thankYouMssg, '"THANK YOU FOR YOUR FEEDBACK" message is not displayed').to.be.true;
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Then(/^I verify the (plus|minus) button for adults is (disabled|enabled)$/, async (button, status) => {
+
+    //     switch (button) {
+    //         case 'plus':
+    //             const isPlusEnabled = await hotels.isPlusEnabled();
+    //             switch (status) {
+    //                 case 'disabled':
+    //                     expect(isPlusEnabled, 'Plus button is still enabled').to.equal('true');
+    //                     break;
+    
+    //                 case 'enabled':
+    //                     expect(isPlusEnabled, 'Plus button is not enabled').to.equal('false');
+    //                     break;
+    
+    //                 default:
+    //                     break;
+    //             }
+    //             break;
+    
+    //         case 'minus':
+    //             const isMinusEnabled = await hotels.isMinusEnabled();
+    //             switch (status) {
+    //                 case 'disabled':
+    //                     expect(isMinusEnabled, 'Minus button is still enabled').to.equal('true');
+    //                     break;
+    
+    //                 case 'enabled':
+    //                     expect(isMinusEnabled, 'Minus button is not enabled').to.equal('false');
+    //                     break;
+                
+    //                 default:
+    //                     break;
+    //             }
+    
+    //         default:
+    //             break;
+    //     }
+    
+    // })
